65ab7c15c26f0fb41ce287736bd86007f07ec757
HeinrichFennec <heinrich@fennecgroup.co.za>
2025-06-11 18:39:30 +0200

    Update ner.py

--- scripts/ner.py
+++ scripts/ner.py
@@ -1,6 +1,26 @@
 from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+# Amharic (amh)                               these are the known languages in the masakhaner dataset
+# Hausa (hau)
+# Igbo (ibo)
+# Kinyarwanda (kin)
+# Luganda (kin)
+# Luo (luo)
+# Nigerian-Pidgin (pcm)
+# Swahili (swa)
+# Wolof (wol)
+# Yoruba (yor)
 
-languages = ['zul', 'xho', 'tsn'] #zulu, xhosa, setswana 
+#my task for research
+#  Pivot Language Selection: Why do mutual intelligibility metrics fail to predict
+# embedding quality (e.g., isiXhosa→isiZulu outperforms the reverse)? Data
+# Efficiency: How do conjunctive languages (e.g., isiZulu) scale with data size
+# compared to disjunctive languages (e.g., Sesotho sa Leboa) in downstream tasks
+# like NER?
+
+languages = ["amh", 'hau', 'ibo', 'kin', 'lug', 'luo', 'pcm', 'swa', 'wol',  'yor' ] #zulu, xhosa, setswana 
+conjunctive_languages = ['kin', 'lug', 'swa']  # languages with conjunctive properties
+disjunctive_languages = ['amh', 'hau', 'ibo', 'luo', 'pcm', 'wol', 'yor']  # languages with disjunctive properties
 
 
 datasets = {}
